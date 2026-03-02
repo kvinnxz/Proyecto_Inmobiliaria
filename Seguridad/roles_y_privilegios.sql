d5eaa32324235bf5807aa45660277155a3670c78
kvinnxz <keviin9856@gmail.com>
2026-03-02 17:13:56 -0500

    feat: :sparkles: Se agregan usuarios

--- Seguridad/roles_y_privilegios.sql
+++ Seguridad/roles_y_privilegios.sql
@@ -9,3 +9,9 @@ GRANT SELECT ON InmobiliariaDB.Reportes_Pendientes TO 'contador_rol';
 
 CREATE USER IF NOT EXISTS 'agente_pedro'@'localhost' IDENTIFIED BY 'Password123!';
 GRANT 'agente_rol' TO 'agente_pedro'@'localhost';
+
+CREATE USER IF NOT EXISTS 'admin_carlos'@'localhost' IDENTIFIED BY 'AdminPass456!';
+GRANT 'admin_rol' TO 'admin_carlos'@'localhost';
+
+CREATE USER IF NOT EXISTS 'contador_ana'@'localhost' IDENTIFIED BY 'ContaPass789!';
+GRANT 'contador_rol' TO 'contador_ana'@'localhost';
